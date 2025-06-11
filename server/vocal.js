cd9d58e44e050281adaf522c11e2e6c6ca1781cf
Yan_ice <yan2364728692@gmail.com>
2025-06-11 20:30:28 +0800

    no self

--- server/vocal.js
+++ server/vocal.js
@@ -293,7 +293,7 @@ io.on("connection", (socket) => {
 
     // 给客户端返回所有初始化信息
     callback({
-      existingUsers: Array.from(room.users.keys()),
+      existingUsers: Array.from(room.users.keys().filter((id)=>{return id!=socket.data.userId;})),
     });
   });
 
