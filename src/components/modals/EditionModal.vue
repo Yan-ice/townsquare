fe4f6ad6af493904e986b400336a6e49c16f7340
Yan-ice <yan2364728692@gmail.com>
2026-05-31 19:13:33 +0800

    script

--- src/components/modals/EditionModal.vue
+++ src/components/modals/EditionModal.vue
@@ -111,7 +111,7 @@ export default {
     },
     async loadScriptsFromServer() {
       try {
-        const url = "https://yanice.online/api/edition_list"; // Flask 路由
+        const url = "https://yanice.online/script/api/edition_list"; // Flask 路由
         const res = await fetch(url, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
